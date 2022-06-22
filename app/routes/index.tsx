2dbe14f1125ce9792968bc62425dab09e4292a47
roninfrodsham <roninfrodsham@gmail.com>
2022-06-22 13:45:03 +0100

    update copy

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,7 +2,7 @@ export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
       <h1>Remix on Cloudflare</h1>
-      <p>Hello from CloudFlare pages.</p>
+      <p>Hello from CloudFlare pages with the help of GitHub</p>
     </div>
   );
 }
